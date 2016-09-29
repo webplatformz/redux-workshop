21096ebfd9e267bdc9318388a792708191c4a7c7
nick <nick.payne89@gmail.com>
2016-09-29 08:40:18 +0200

    pure reducers exercise

--- src/reducers.js
+++ src/reducers.js
@@ -1,8 +1,8 @@
-import { ADD_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER, REMOVE_TODO } from './actions';
+import { ADD_TODO } from './actions';
 
 const initialState = {
     todos: [{
-        text: 'hello youse',
+        text: 'write some pure reducers!',
         id: 1,
         completed: false
     }],
@@ -13,38 +13,15 @@ let nextId = 2;
 function TodoApp(state = initialState, action) {
     switch (action.type) {
         case ADD_TODO:
-            return Object.assign({}, state, {
-                todos: [
-                    ...state.todos,
-                    {
-                        text: action.text,
-                        completed: false,
-                        id: nextId++
-                    }
-                ]
+            state.todos.push({
+                text: action.text,
+                completed: false,
+                id: nextId++
             });
-        case TOGGLE_TODO:
-            return Object.assign({}, state, {
-                todos: state.todos.map((todo) => {
-                    if (action.id === todo.id) {
-                        return Object.assign({}, todo, {
-                            completed: !todo.completed
-                        });
-                    }
-
-                    return todo;
-                })
-            });
-        case REMOVE_TODO:
-            return Object.assign({}, state, {
-                todos: state.todos.filter(t => t.id !== action.id)
-            });
-        case SET_VISIBILITY_FILTER:
-            return Object.assign({}, state, {
-                visibilityFilter: action.filter
-            });
+            break;
         default:
             return state;
+
     }
 }
 
--- test/reducersTest.js
+++ test/reducersTest.js
@@ -6,7 +6,7 @@ describe('ToDoAppReducers', function() {
 
     const initialState = {
         todos: [{
-            text: 'hello youse',
+            text: 'write some pure reducers!',
             id: 1,
             completed: false
         }],
@@ -17,40 +17,24 @@ describe('ToDoAppReducers', function() {
         deepFreeze(initialState);
     });
 
-    it('should return the initial state when no state is given', function() {
+    it('should return the initial state when no state and action is given', function() {
         expect(reducer(undefined, {})).toEqual(initialState);
     });
 
     describe('the todo reducer', function() {
 
-        it('should add a todo when dispatching ADD_TODO', function() {
+        xit('should add a todo when dispatching ADD_TODO', function() {
             let newState = reducer(initialState, addTodo('a new todo'));
 
             expect(newState.todos.length).toBe(2);
             expect(newState.todos[1].text).toEqual('a new todo');
         });
 
-        it('should toggle a todo when dispatching TOGGLE_TODO', function() {
+        xit('should toggle a todo when dispatching TOGGLE_TODO', function() {
             var id = 1;
             let newState = reducer(initialState, toggleTodo(id));
 
             expect(newState.todos[0].completed).toBe(true);
-        });
-
-        it('should remove a todo when dispatching REMOVE_TODO', function() {
-            var id = 1;
-            let newState = reducer(initialState, removeTodo(id));
-
-            expect(newState.todos.length).toBe(0);
-        });
-    });
-
-    describe('the visibility filter reducer', function() {
-
-        it('should set the new visibility filter when dispatching SET_VISIBILITY_FILTER', function() {
-            let newState = reducer(initialState, setVisibilityFilter('SHOW_COMPLETED'));
-
-            expect(newState.visibilityFilter).toEqual('SHOW_COMPLETED');
         });
     });
 
